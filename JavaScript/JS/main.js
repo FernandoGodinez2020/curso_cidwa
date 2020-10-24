b18f5594d92b086ea6618d6a853e59e116ca76ba
Fernando Godinez <316720171@pcpuma.acatlan.unam.mx>
2020-10-24 11:07:05 -0500

    Primeros pasos

---
+++ JavaScript/JS/main.js
@@ -0,0 +1,47 @@
+//console.log("hola")
+//let saludo = "sadasd sadasd" /* utilizamos la valiable let ya que es la más nueva*/
+//var numero = 60.5626//
+
+//hola = "Konichiwa :3"
+//console.log(hola) /*en este imprime el konichiwa*/
+//hola = 60;
+
+//console.log(hola) /*en este caso imprime el 60 ya que está justo debajo de ese 60*/
+
+//hello = saludo + "string random" + numero;
+
+//console.log(hello) */
+/*
+let numeroUno = 10;
+let numeroDos = 20;
+let operación
+
+operacion = numeroUno + numeroDos
+
+/* 
++ suma
+- resta
+/ división
+* multiplicación
+*/
+
+
+/*
+console.log(numeroUno + numeroDos)
+
+console.log(operacion)
+
+numeroUno + 1;
+//estos dos comandos son exactamente lo mismo
+numeroUno++;
+*/
+
+let uno = "uno"
+
+{
+    //let uno = "dos" //esto se denomina scope, lo de afuera no accede a lo de adentro
+    console.log(uno)
+    {
+        let uno = "tres" 
+    }
+}
